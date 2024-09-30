8178f929a54a118c5dc693d1a46a77000bbc82fa
Mishal Shah <mishal_shah@apple.com>
2024-09-29 22:39:39 -0700

    Soundness workflow
    This includes multiple checks:
    * API breakage check
    * Documentation check
    * Unacceptable language check
    * License headers check
    * Broken symlinks check
    * Format check
    * Shell check
    
    This was copied from the swift-nio repository to help centralize the workflows.

---
+++ .github/workflows/scripts/check-broken-symlinks.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ===----------------------------------------------------------------------===//
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2024 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See https://swift.org/LICENSE.txt for license information
+# See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ===----------------------------------------------------------------------===//
+
+set -euo pipefail
+
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
+fatal() { error "$@"; exit 1; }
+
+log "Checking for broken symlinks..."
+num_broken_symlinks=0
+while read -r -d '' file; do
+  if ! test -e "./${file}"; then
+    error "Broken symlink: ${file}"
+    ((num_broken_symlinks++))
+  fi
+done < <(git ls-files -z)
+
+if [ "${num_broken_symlinks}" -gt 0 ]; then
+  fatal "❌ Found ${num_broken_symlinks} symlinks."
+fi
+
+log "✅ Found 0 symlinks."
---
+++ .github/workflows/scripts/check-unacceptable-language.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# ===----------------------------------------------------------------------===//
+#
+# This source file is part of the Swift.org open source project
+#
+# Copyright (c) 2024 Apple Inc. and the Swift project authors
+# Licensed under Apache License v2.0 with Runtime Library Exception
+#
+# See https://swift.org/LICENSE.txt for license information
+# See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+#
+# ===----------------------------------------------------------------------===//
+
+set -euo pipefail
+
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
+fatal() { error "$@"; exit 1; }
+
+test -n "${UNACCEPTABLE_WORD_LIST:-}" || fatal "UNACCEPTABLE_WORD_LIST unset"
+
+log "Checking for unacceptable language..."
+unacceptable_language_lines=$(git grep \
+  -i -I -w \
+  -H -n --column \
+  -E "${UNACCEPTABLE_WORD_LIST// /|}" | grep -v "ignore-unacceptable-language"
+) || true | /usr/bin/paste -s -d " " -
+
+if [ -n "${unacceptable_language_lines}" ]; then
+    fatal " ❌ Found unacceptable language:
+${unacceptable_language_lines}
+"
+fi
+
+log "✅ Found no unacceptable language."
