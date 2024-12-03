bd4321fa0bc9ea7ae44991847d09096e8db77b3f
Adam Ward <award999@apple.com>
2024-12-03 08:39:17 -0500

    Add license headers

--- .github/workflows/scripts/windows/install-vsb.ps1
+++ .github/workflows/scripts/windows/install-vsb.ps1
@@ -1,3 +1,14 @@
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift.org open source project
+##
+## Copyright (c) 2024 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See https://swift.org/LICENSE.txt for license information
+## See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+##
+##===----------------------------------------------------------------------===##
 $VSB='https://download.visualstudio.microsoft.com/download/pr/5536698c-711c-4834-876f-2817d31a2ef2/c792bdb0fd46155de19955269cac85d52c4c63c23db2cf43d96b9390146f9390/vs_BuildTools.exe'
 $VSB_SHA256='C792BDB0FD46155DE19955269CAC85D52C4C63C23DB2CF43D96B9390146F9390'
 Set-Variable ErrorActionPreference Stop
--- .github/workflows/scripts/windows/swift/install-swift-nightly-6.0.ps1
+++ .github/workflows/scripts/windows/swift/install-swift-nightly-6.0.ps1
@@ -1,3 +1,14 @@
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift.org open source project
+##
+## Copyright (c) 2024 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See https://swift.org/LICENSE.txt for license information
+## See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+##
+##===----------------------------------------------------------------------===##
 . $PSScriptRoot\install-swift.ps1
 
 $SWIFT_RELEASE_METADATA='http://download.swift.org/swift-6.0-branch/windows10/latest-build.json'
