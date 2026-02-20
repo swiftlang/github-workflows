bca81f75d7d1ae2474f1213169aab56dcad8961f
Mishal Shah <shahmishal@users.noreply.github.com>
2026-02-20 10:45:28 -0800

    Revert Use local copy on action invocation (#239)
    * Revert "Add symlinks for backwards compatability (#238)"
    
    This reverts commit 27ad4acfa502b0cb8b901532e8294a14b7273988.
    
    * Revert "Use local copy on action invocation (#236)"
    
    This reverts commit c6fcbaf70a845126ec165107a2d509bda17a2fd1.

--- .github/workflows/scripts/check-unacceptable-language.sh
+++ .github/workflows/scripts/check-unacceptable-language.sh
@@ -1,1 +1,39 @@
-../../actions/check-unacceptable-language/check-unacceptable-language.sh
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift.org open source project
+##
+## Copyright (c) 2024 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See https://swift.org/LICENSE.txt for license information
+## See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+##
+##===----------------------------------------------------------------------===##
+
+set -euo pipefail
+
+log() { printf -- "** %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
+fatal() { error "$@"; exit 1; }
+
+test -n "${UNACCEPTABLE_WORD_LIST:-}" || fatal "UNACCEPTABLE_WORD_LIST unset"
+
+unacceptable_language_lines=
+if [[ -f .unacceptablelanguageignore ]]; then
+    log "Found unacceptable language ignore file..."
+    log "Checking for unacceptable language..."
+    unacceptable_language_lines=$(tr '\n' '\0' < .unacceptablelanguageignore | xargs -0 -I% printf '":(exclude)%" '| xargs git grep -i -I -w -H -n --column -E "${UNACCEPTABLE_WORD_LIST// /|}" -- ':!*.unacceptablelanguageignore' | grep -v "ignore-unacceptable-language") || true | /usr/bin/paste -s -d " " -
+else
+    log "Checking for unacceptable language..."
+    unacceptable_language_lines=$(git grep -i -I -w -H -n --column -E "${UNACCEPTABLE_WORD_LIST// /|}" | grep -v "ignore-unacceptable-language") || true | /usr/bin/paste -s -d " " -
+fi
+
+if [ -n "${unacceptable_language_lines}" ]; then
+    fatal " ❌ Found unacceptable language:
+${unacceptable_language_lines}
+"
+fi
+
+
+log "✅ Found no unacceptable language."
